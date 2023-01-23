4a382fddd7166fdb22eee3fa0b2b73e130e72bae
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-01-23 18:50:01 +0200

    add component Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,22 @@
 import React, { Component } from 'react'
+import Searchbar from './Searchbar/Searchbar';
 
 
-export default class App extends Component {
+export class App extends Component {
   state = {
-    picture: null
+    searchText: ''
+  };
+
+  getSearchSubmit = searchText => {
+    this.setState({ searchText })
   };
 
 
   render() {
     return (
       <div>
-        
+        <Searchbar onSubmit={this.getSearchSubmit} />
       </div>
     )
   }
 }
-
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,18 +1,54 @@
 
-const URL = "https://pixabay.com/api/?q=cat&page=1&key=31092155-fdd6914219543248b658a821f&image_type=photo&orientation=horizontal&per_page=12"
+// const URL = "https://pixabay.com/api/?q=cat&page=1&key=31092155-fdd6914219543248b658a821f&image_type=photo&orientation=horizontal&per_page=12"
 
-{/* <header class="searchbar">
-  <form class="form">
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
 
-    <input
-      class="input"
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
+import React, { Component } from 'react'
+
+export default class Searchbar extends Component {
+    state = { 
+        searchName: ''
+    }
+
+
+    handleNameChange = event => { 
+        this.setState({
+            searchName: event.currentTarget.value.toLowerCase()
+        });
+    }
+
+    submitForm = event => { 
+        event.preventDefault();
+
+        if (this.state.searchName.trim() === '') {
+            alert('Something go wrong. Try one more time')
+            return
+        }
+        this.props.onSubmit(this.state.searchName);
+        this.setState({ searchName: ''})
+    }
+
+  render() {
+    return (
+        <header className="searchbar">
+
+            <form className="form" onSubmit={this.submitForm}>
+                
+        <button type="submit" className="button">
+            <span className="button-label">Search</span>
+        </button>
+
+        <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.searchName}
+            onChange={this.handleNameChange}              
     />
-  </form>
-</header> */}
+            </form>
+        </header>
+    )
+  }
+}
+
