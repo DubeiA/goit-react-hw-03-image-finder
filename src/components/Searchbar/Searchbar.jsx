77c09e019b20715f3127d8fd493494d13c95e4a6
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-01-23 18:08:36 +0200

    add folders and files what will need to do task

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,18 @@
+
+const URL = "https://pixabay.com/api/?q=cat&page=1&key=31092155-fdd6914219543248b658a821f&image_type=photo&orientation=horizontal&per_page=12"
+
+{/* <header class="searchbar">
+  <form class="form">
+    <button type="submit" class="button">
+      <span class="button-label">Search</span>
+    </button>
+
+    <input
+      class="input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header> */}
